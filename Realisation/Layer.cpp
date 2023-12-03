1438ff1f7e6d7da8703e90c623480d1e17f25e88
MrS-E <simeon.stix@gmx.ch>
2023-12-03 09:30:47 +0100

    added function to calculate neuron output

--- Realisation/Layer.cpp
+++ Realisation/Layer.cpp
@@ -8,3 +8,9 @@ Layer::Layer(int inputSize, int neuronCount) {
     neurons.resize(neuronCount);
     weights.resize(neuronCount, std::vector<double>(inputSize));
 }
+
+void Layer::calc_neuron_outputs(std::vector<double> &inputs) {
+    for (size_t i = 0; i < neurons.size(); ++i) {
+        neurons[i].calc_out(inputs, weights[i]);
+    }
+}
