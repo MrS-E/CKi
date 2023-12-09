0221939151449c91ea1a65ec9087c6ca80fad5ba
MrS-E <simeon.stix@gmx.ch>
2023-12-09 17:57:15 +0100

    forgot to init weights

--- Realisation/Layer.cpp
+++ Realisation/Layer.cpp
@@ -7,6 +7,7 @@
 Layer::Layer(int inputSize, int neuronCount) {
     neurons.resize(neuronCount);
     weights.resize(neuronCount, std::vector<double>(inputSize));
+    init_weights();
 }
 
 void Layer::calc_neuron_outputs(const std::vector<double> &inputs) {
@@ -28,6 +29,15 @@ void Layer::update_weights(const std::vector<double> &inputs, std::vector<double
     for (size_t i = 0; i < neurons.size(); ++i) {
         for (size_t j = 0; j < weights[i].size(); ++j) {
             weights[i][j] += learningRate * deltas[i] * inputs[j];
+        }
+    }
+}
+
+void Layer::init_weights() {
+    srand(static_cast<unsigned>(time(0)));
+    for (size_t i = 0; i < neurons.size(); ++i) {
+        for (double & j : weights[i]) {
+            j = (rand() % 2000 - 1000) / 1000.0;  // Random values between -1 and 1
         }
     }
 }
--- Realisation/Layer.h
+++ Realisation/Layer.h
@@ -14,6 +14,7 @@ public:
     void calc_neuron_outputs(const std::vector<double>& inputs);
     std::vector<double> get_neuron_outputs();
     void update_weights(const std::vector<double>& inputs, std::vector<double>& deltas, double learningRate);
+    void init_weights();
 
     std::vector<std::vector<double>> weights;
     std::vector<Neuron> neurons;
