42943f2b3dddabb02fee61afe02d2f780cc1c6a7
MrS-E <simeon.stix@gmx.ch>
2024-02-22 00:24:12 +0100

    remove files

--- Realisation/test/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by MrS-E on 11/02/2024.
-//
-#include <gtest/gtest.h>
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
